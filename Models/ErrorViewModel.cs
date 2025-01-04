25e43c7e3ed602a526a6ac51da4bf15b66a5ab76
LEANDRO <crazyg4ng@gmail.com>
2025-01-03 23:33:16 -0300

    Projeto criado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prefeituras.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
