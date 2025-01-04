70186d53af899f0f88721df97f8a6c1fe93526b7
LEANDRO <crazyg4ng@gmail.com>
2025-01-04 19:11:55 -0300

    tradução filtro

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,40 @@
-﻿let table = new DataTable('#table-contatos');
+﻿$(document).ready(function () {
+    $('#table-contatos').DataTable({
+        "ordering": true,
+        "pading": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Monstar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros pro pagina",
+            "sLoadingRecords": "Carregando...",
+            "sPrecessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginato": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+
+    });
+});
 
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
 
 setTimeout(function () {
-    $('.alert').hide('hide');
+    $('.alert').fadeOut('hide');
 }, 5000);
 
